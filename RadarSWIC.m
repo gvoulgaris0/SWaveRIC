1bff552b7582f742a43be2261c07e565848f982b
Douglas Cahl <48590543+dougcahl@users.noreply.github.com>
2021-07-31 10:36:00 -0400

    Add files via upload

--- RadarSWIC.m
+++ RadarSWIC.m
@@ -4,57 +4,58 @@
 function [Df,Sf,Sfth,Fs,Hsw,Ths,Ths_err] = RadarSWIC(freq,PXY1,PXY2,f,fr,aw,sigma,fc,jswaproll,beam1,beam2,d,th,wspd,switc1)
 %% Description
 %  This function uses Doppler spectra from two beams from two VHF/HF radar 
-%  sites, or two beams from one VHF/HF radar site, identifies its 2nd order
-%  continuum and inverts it to create an ocean wave spectrum.
+%  sites, or two beams from one VHF/HF radar site, identifies the 2nd order
+%  continuum in each spectrum and inverts it to create an ocean wave spectrum.
 %
 %  For more details see:
 %
 %  Al-Attabi, Z., Voulgaris G., and Conley D.,(2021):Evaluation and Validation of
-%  HF Radar Swell and Wind wave Inversion, Journal of
-%  Oceanic and Atmopsheric Technology, xx: yyyy-yy
+%  HF Radar Swell and Wind wave Inversion, Journal of Oceanic and 
+%  Atmopsheric Technology, https://doi.org/10.1175/JTECH-D-20-0186
 %
 % The function:
 %
 %  1. Identifies the 2nd order sides of Doppler spectrum and creates the
-%     weighed and normalized by the 1st order spectra (Rw)
+%     weighted and normalized by the 1st order spectra (Rw)
 %  2. Inverts Rw to wind wave spectrum (Sfw) using the appropiate calibration
 %     coefficient (aw)
 %  3. Estimates the swell part of the spectrum using Lipa et al 1981 method.
 %  4. Inverts the swell part of Rw to a spectrum using a Gaussian function
-%     with width sigma uisng Hs, fs, ths from lipa's inversion method
-%  5. Reconstruct total wave spectrum Sf = Sfs+Sfw
-%  6. Estimate the 2D directioanl wave spectrum
+%     with width sigma using Hs, fs, ths from Lipa's inversion method
+%  5. Reconstructs total wave spectrum Sf = Sfs+Sfw
+%  6. Estimates the 2D directioanl wave spectrum
 %
 %  Inversion parameters are set in file:  ConfigRSWIC.m
 %% Inputs
-%  PXY1         - Doppler spectrum from beam1                        (in dB)
-%  PXY2         - Doppler spectrum from beam2                        (in dB)
-%  freq         - frequency of the radar Doppler spectrum            (in Hz)
-%  f            - Wave frequency array for inversion                 (in Hz)
-%  fr           - HF radar operating frequency                       (in Mhz)
-%  aw           - wind-wave regression coefficient - used for wind-wave inversion
-%  sigma        - Gaussian swell spectrum width (empirical fit) - used for swell inversion
-%  fc           - frequency separating, separating swell from wind waves region; (in Hz)
-%                 It limits swell peak freq. below this value; it is also used by the hybrid model ratio test (see Allatabi et al.,2021).
-%  jswaproll    - if =1 [default] apply JONSWAP roll off below last Rw value
-%  beam1        - radar beam angle at which PXY1 is collected        (in deg)
-%  beam2        - radar beam angle at which PXY2 is collected        (in deg)
-%  d            - depth at which location radar data is used         (in m)
-%  th           - range of directional distribution (-180 to 180)    (in deg)
-%  wspd         - wind speed (at 10m elevation)                      (in m/s)
-%  switc1       - switch used for avoid Bragg peak (switc1=1), or include all data below fc (switc1=999)
+%  PXY1      - Doppler spectrum from beam1                        (in dB)
+%  PXY2      - Doppler spectrum from beam2                        (in dB)
+%  freq      - frequency of the radar Doppler spectrum            (in Hz)
+%  f         - Wave frequency array for inversion                 (in Hz)
+%  fr        - HF radar operating frequency                       (in Mhz)
+%  aw        - wind-wave regression coefficient - used for wind-wave inversion
+%  sigma     - Gaussian swell spectrum width (empirical fit) - used for swell inversion
+%  fc        - frequency separating, separating swell from wind waves region; (in Hz)
+%              It limits swell peak freq. below this value; it is also used by the hybrid model ratio test (see Allatabi et al.,2021).
+%  jswaproll - if =1 [default] apply JONSWAP roll off below last Rw value
+%  beam1     - radar beam angle at which PXY1 is collected        (in deg)
+%  beam2     - radar beam angle at which PXY2 is collected        (in deg)
+%  d         - depth at which location radar data is used         (in m)
+%  th        - range of directional distribution (-180 to 180)    (in deg)
+%  wspd      - wind speed (at 10m elevation)                      (in m/s)
+%  switc1    - switch used for avoid Bragg peak (switc1=1), or include all data below fc (switc1=999)
 %% Outputs
-%  Df        -  Wave directional spectrum (D(f))
+%  Df        - Wave direction as function of wave frequency (D(f), in deg)
 %  Sf        - 1D- total wave energy spectral density (S(f), m2/Hz)
 %  Sfth      - 2D- total directional wave spectrum (S(f,theta), m2/Hz/deg)
 %  Fs        - Swell wave frequency
-%  Hsw       - Swell root mean square wave height (Swell Hrms)
-%  Ths       - Swell Wave direction
-%  Ths_err   - Estimate of uncertainty in swell direction
+%  Hsw       - Swell root mean square wave height (Swell Hrms, in m)
+%  Ths       - Swell Wave direction (in deg)
+%  Ths_err   - Estimate of uncertainty in swell direction (in deg)
 %% Uses
-%  wn2ndRSWIC.m; specWindRSWIC.m; dopLipaS.m; swell_param_singularities.m;
-%  specSwellRSWIC.m wspecRSWIC_hybrid.m waveparamter_separated_v3.m
-%  Inverted_2D_DirSpec.m
+%  wn2ndRSWIC.m,          specWindRSWIC.m,     
+%  dopLipaS.m,            swell_singular.m,
+%  specSwellRSWIC.m,      wspecRSWIC_hybrid.m, 
+%  Inverted_2D_DirSpec.m, waveparams3.m
 %% Copyright 
 %  2021 Zaid Alattabi, Douglas Cahl, George Voulgaris
 %
